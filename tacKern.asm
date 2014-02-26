3faa82d33ff4cdd6cbe3eea7fe9734ac2dc9adaa
David <pod32g@yahoo.com.mx>
2014-02-25 20:57:34 -0600

    Version 0.0000000001
    Re-written from scratch

--- tacKern.asm
+++ tacKern.asm
@@ -1,19 +1,47 @@
-mov ax, 9ch
-mov ss, ax
-mov sp, 4096d
-mov ax, 7c0h
-mov ds, ax
-;Todo despues de aqui, sera parte del bootloader
-mov ah, 0eh ;display string
-mov al, 37h
-int 10h
- 
-; Endless Loop
- 
-jmp $
- 
- 
- 
-times 510-($-$$) db 0
-dw 0xAA55 ; Standard Computer Signature
- 
+[BITS 16] ;Tell Assembler, its a 16 bit code
+[ORG 0x7C00] ;Origin, tell assembler here will be the code in memory after 
+              ;boot
+
+MOV SI, HelloString
+CALL PrintString 
+
+JMP $
+
+PrintCharacter: ; Procedure to print character
+
+MOV AH, 0x0E ; TELL BIOS WE NEED TO PRINT ON SCREEN
+MOV BH, 0x00 ; PAGE NUMBER
+MOV BL, 0x07 ; TEXT ATTRIBUTE 0x07 LIGHTGREY
+
+INT 0x10 ; CALL VIDEO INTERRUPT
+RET ; RETURN TO CALLING PROCEDURE
+
+PrintString: ; PRINT STRING ON SCREEN
+
+next_character: ;FETCH CHARACTER FROM STRING
+
+MOV AL, [SI] ; GET BYTE FROM STRING AND STORE IN AL REGISTER
+INC SI ; INCREMENT SI POINTER
+OR AL, AL ; CHECK IF VALUE IN AL IS ZERO
+JZ exit_function ; IF END THEN RETURN
+CALL PrintCharacter
+JMP next_character
+
+getc: ; read character into al
+
+MOV ah, 1h
+
+INT 21h
+
+RET 
+
+exit_function:
+RET
+
+; MAIN STRING
+
+HelloString db 'Hello World', 0 
+
+
+TIMES 510-($-$$) db 0 ; Fill the rest of the sector with 0
+DW 0xAA55 ; Standard boot signature
